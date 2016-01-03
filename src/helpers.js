4025f9fd2022fd7e1aa610fcbd2a241c241174ab
Sabin Tudor <sabin@gmx.us>
2016-01-03 03:24:03 +0200

    Add respaw(new) common helper

--- src/helpers.js
+++ src/helpers.js
@@ -1,18 +1,52 @@
 // deps
 import util from 'util';
+import { spawn } from 'child_process';
+
+// respawn code
+const RESPAWN_CODE = 7; // holy humber!
 
 //
 //  object representation / stringify
 //
-const inspect = (obj) => {
+const inspect = (obj, depth = null) => {
     return util.inspect(obj, {
         showHidden: true,
         colors: true,
-        depth: null // go deep!
+        depth// go deep!
+    });
+};
+
+//
+//  respawn requester
+//  exit process with custom code
+//
+const respawnMe = (code = RESPAWN_CODE) => {
+    process.exit(code);
+};
+
+//
+//  respawner
+//
+const respawner = (app, args = []) => {
+    if (!app) {
+        throw new Error('Need app/process to spawn, bro!');
+    }
+
+    // spawn it
+    const proc = spawn(app, args, { stdio: 'inherit' });
+
+    // when this process gets terminated (as user intends)
+    // ...spawn again
+    proc.on('close', (code) => {
+        // console.log('close via code @ ', code);
+        if (code === RESPAWN_CODE) respawner(app, args);
     });
 };
 
 // exports
 module.exports = {
-    inspect
+    inspect,
+
+    respawner,
+    respawnMe
 };
